9bc668d865d7e5f1a74bde5935e12c90704aaa7d
mjfunny <mj98986@163.com>
2017-06-05 23:51:25 +0800

    edit user form

--- src/pages/user/UserModalButton.jsx
+++ src/pages/user/UserModalButton.jsx
@@ -1,5 +1,5 @@
 import React,{ Component } from 'react';
-import { Button, Modal } from 'antd';
+import { Button, Modal, Form } from 'antd';
 import { connect } from 'dva';
 
 class UserModalButton extends Component{ 
